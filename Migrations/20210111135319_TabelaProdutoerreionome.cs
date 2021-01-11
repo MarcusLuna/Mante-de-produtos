bd97eb43060e226ae4b839804c6480684c0e3902
MarcusLuna <30804998+MarcusLuna@users.noreply.github.com>
2021-01-11 11:35:59 -0300

    Add files via upload

---
+++ Migrations/20210111135319_TabelaProdutoerreionome.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CursoMV.Migrations
+{
+    public partial class TabelaProdutoerreionome : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Qauntidade",
+                table: "Produtos",
+                newName: "Quantidade");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Quantidade",
+                table: "Produtos",
+                newName: "Qauntidade");
+        }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMV.Models
+{
+    public class Produto
+    {
+
+        public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+
+        [Range(1,100, ErrorMessage ="Valor tem que está entra 1 e 100" )]
+        public int Quantidade { get; set; }
+        public int CategoriaId { get; set; }
+
+        public Categoria Categoria { get; set; }
+
+    }
+}
